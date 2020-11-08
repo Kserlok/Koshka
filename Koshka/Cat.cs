cf1a24e3aadce13f2bb3961c18a8a8bde32f72cc
Kserlok <experiancullum@gmail.com>
2020-11-08 13:52:34 +1000

    Добавьте файлы проекта.

---
+++ Koshka/Cat.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koshka
+{
+    class Cat
+    {
+        byte _hungryStatus;
+        public event EventHandler HungryStatusChanged;
+        public Cat(string name, DateTime birthday)
+        {
+            Name = name;
+            BirthDay = birthday;
+            Task.Run(LifeCircle);
+        }
+    public string Name
+    {
+         get; set;
+    }
+        public DateTime BirthDay
+        {
+            get; set;
+        }
+        public void MakeNoise()
+        {
+            Console.WriteLine(Name + " мяукает");
+        }
+
+        public int GetAge()
+        {
+            return (DateTime.Today - BirthDay).Days / 365;
+        }
+
+        public byte HungryStatus
+        {
+            get { return _hungryStatus; }
+            set
+            {
+                byte status = value;
+                if (status < 0)
+                {
+                    status = 0;
+                }
+                else if (status > 100)
+                {
+                    status = 100;
+                }
+                else
+                    _hungryStatus = value;
+                if (_hungryStatus != value)
+                {
+                    HungryStatusChanged?.Invoke(this, null);
+                }
+            }
+        }
+        public void Feed(byte needFood)
+        {
+            HungryStatus += needFood;
+        }
+
+        public void GetStatus()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine($"Возраст: {GetAge()}");
+            if (HungryStatus < 10)
+            { 
+               Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Кот умирает от голода"); 
+            }
+            else if (HungryStatus >= 10 && HungryStatus <= 40)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Кот очень голоден");
+            }
+            else if (HungryStatus > 40 && HungryStatus <= 70)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Кот хочет кушать");
+            }
+            else if (HungryStatus > 70 && HungryStatus <= 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Кот не против перекусить");
+            }
+            else if (HungryStatus > 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Кот недавно поел");
+            }
+            Console.ResetColor();
+        }
+
+        public async Task LifeCircle()
+        {
+            await Task.Delay(100);
+            HungryStatus -= 10;
+            await LifeCircle(); ;
+        }
+    }
+}
+
+
---
+++ Koshka/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.Security.Cryptography;
+
+namespace Koshka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Кокос", new DateTime(2015, 02, 10));
+            cat.MakeNoise();
+            Console.WriteLine($"Коту по имени {cat.Name} уже {cat.GetAge()} лет");
+            // cat.HungryStatus = 500;
+
+            Cat cat2 = new Cat("Фисташка", new DateTime(2019, 11, 1));
+            cat2.MakeNoise();
+            Console.WriteLine($"Коту по имени {cat2.Name} уже {cat2.GetAge()} лет");
+
+            CatSmartHouse catHouse = new CatSmartHouse(900);
+            catHouse.AddCat(cat);
+            catHouse.AddCat(cat2);
+
+            Console.ReadLine();
+        }
+    }
+}
